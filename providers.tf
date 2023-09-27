93392614b55d71ab3b746e3466b4e25ca5bb79cb
alec-ernstrom <alec.ernstrom@gmail.com>
2023-09-27 05:12:22 +0000

    #20 Configuration Drift Testing

--- providers.tf
+++ providers.tf
@@ -6,13 +6,13 @@ terraform {
 #    }
 #  }
   required_providers {
-    random = {
-      source = "hashicorp/random"
-      version = "3.5.1"
-    }
+#    random = {
+#      source = "hashicorp/random"
+#      version = "3.5.1"
+#  }
     aws = {
       source = "hashicorp/aws"
-      version = "5.17.0"
+      version = "5.17.0" 
     }
   }
 }
