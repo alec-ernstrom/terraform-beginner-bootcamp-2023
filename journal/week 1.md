b243cbb9e84ee17973bddd96a7d3bd8524e85db6
alec-ernstrom <alec.ernstrom@gmail.com>
2023-09-27 01:44:41 +0000

    #18 Change structure of root module

--- journal/week 1.md
+++ journal/week 1.md
@@ -13,3 +13,32 @@ Out Root Module Structure is as follows:
   - ``README.md`` - required for root modules
 
 [Standard Module Strucuture](https://developer.hashicorp.com/terraform/language/modules/develop/structure)
+
+## ToC Markdown Generator
+
+https://ecotrust-canada.github.io/markdown-toc/
+
+## Terraform Cloud Variables
+
+In Terraform we can set 2 kinds of variables:
+
+- Environment Variables - ``These are set in your bash terminal``
+- Terraform Variables - ``These are set within a file like 'tfvars'``
+
+### Loading Terraform Input Variables
+
+[Terraform Input Variables](https://developer.hashicorp.com/terraform/language/values/variables)
+
+We can use the `-var` flag to set an input variable or override a variable in the tfvars file.
+
+### var-file flag
+
+- TODO: document this functionality for Terraform Cloud
+
+### terraform.tfvars
+
+This is the default file to load in Terraform variables in bulk
+
+### auto.tfvars
+
+- TODO: document which Terraform Variables take precedence 
--- main.tf
+++ main.tf
@@ -1,30 +1,3 @@
-terraform {
-  cloud {
-    organization = "alec_terra_org"
-    workspaces {
-      name = "terra-house-1"
-    }
-  }
-  required_providers {
-    random = {
-      source = "hashicorp/random"
-      version = "3.5.1"
-    }
-    aws = {
-      source = "hashicorp/aws"
-      version = "5.17.0"
-    }
-  }
-}
-
-provider "aws" {
-  # Configuration options
-}
-
-provider "random" {
-  # Configuration options
-}
-
 # Random String Documentation: https://registry.terraform.io/providers/hashicorp/random/latest/docs/resources/string
 resource "random_string" "bucket_name" {
   upper            = false
@@ -32,14 +5,13 @@ resource "random_string" "bucket_name" {
   special          = false
 }
 
-output "random_bucket_name" {
-  value=random_string.bucket_name.result
-}
-
 # Commands for bucket creation: https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/s3_bucket
 
 resource "aws_s3_bucket" "example" {
   # Bucket Name Rules: https://docs.aws.amazon.com/AmazonS3/latest/userguide/bucketnamingrules.html
   bucket = random_string.bucket_name.result
-}
 
+  tags = {
+    UserUuid = var.user_uuid
+  }
+}
