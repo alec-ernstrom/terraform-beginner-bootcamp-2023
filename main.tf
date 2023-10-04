3bb728d1bdab68931ebc1e0bba52a610c1201f58
alec-ernstrom <144366294+alec-ernstrom@users.noreply.github.com>
2023-10-04 13:48:34 -0700

    34 terratowns mock server (#35)
    * #34 Copy  Terratowns Mock Server into repo. Move bin directories into main bin
    
    * #34 Document Terratowns mock ser4ver. Add validation for inclusion
    
    * adjustments
    
    * #34 make sure  that all our endpoints work correctly

--- main.tf
+++ main.tf
@@ -5,4 +5,9 @@ module "terrahouse_aws" {
   index_html_filepath = var.index_html_filepath
   error_html_filepath = var.error_html_filepath
   content_version = var.content_version
+  assets_path = var.assets_path # this was not here as of 10/4/23 - so was added in to resolve the error popping up
+}
+
+module "terratowns" {
+  source = "./bin/terratowns"
 }
